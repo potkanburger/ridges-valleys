Tried correcting uncorrect gradients by changing the imread and the way data is stored, added tests

exportGradients.py
@@ -10,10 +10,21 @@ def createGradients(listImg):
         img = getImgGrayscale(path+imgPath)
         orig = cv.imread(path+imgPath)
         img = addNeumannBorder(img, 1)
+
+        width = np.size(img, 0)
+        height = np.size(img, 1)
+
         iGx, iGy = gradient(img)
         cv.imwrite(path + "x_gradient_" + imgPath, iGx)
         cv.imwrite(path + "y_gradient_" + imgPath, iGy)
+        cv.imwrite(path + "validator_" + imgPath, img)
+        f = open(path + "x_gradient_" + imgPath +".txt", "w")
 
+        for x in range(1, width - 2):
+            for y in range(1, height - 2):
+                f.write(str(iGx[x, y]))
+            f.write("\n")
+        f.close()
         print(imgPath + " complete !")
 
 def _main():
@@ -21,7 +32,11 @@ def _main():
     # path = "/home/sebastien/Documents/MUNI/IMGS/RV-Graph/"
     path = "./IMGS/"
 
-    imglist = ["generated.jpg"]
+    imglist = []
+    # imglist.append("circular.jpg")
+    imglist.append("smooth_lena.jpg")
+    #imglist.append("ellipses_generated.jpg")
+    # imglist.append("mars1.jpg")
     # imglist.append("gray_binary_fingerprint.jpg")
     # imglist.append("gray_earth.jpg")
     # imglist.append("gray_brain.jpg")

imgGenerator.py
@@ -1,10 +1,7 @@
 import cv2 as cv
 import numpy as np
 import math
-from main import sqr
-
-def createImg(width, height):
-    return np.zeros((height, width, 1), dtype="double")
+from main import *
 
 
 def _main():
@@ -22,8 +19,8 @@ def _main():
                 if tmp_dst < distance:
                     distance = tmp_dst
 
-            value = 255 - distance
-            img[y, x] = max(0.0 , value)
+            value = float(255 - distance)
+            img[y, x] = max(float(0.0), value)
 
     cv.imwrite("./IMGS/ellipses_generated.jpg", img)
 

main.py
@@ -4,8 +4,10 @@ import math
 #import matplotlib.pyplot as plot
 
 def getImgGrayscale(url):
-    return cv.imread(url, 0)
+    return cv.imread(url, 0).astype(np.float)
 
+def createImg(width, height):
+    return np.zeros((height, width, 1), dtype="float")
 
 def sqr(val):
     return pow(val, 2)
@@ -47,14 +49,17 @@ def gradient(src):
     width = np.size(src, 0)
     height = np.size(src, 1)
     #gx = np.zeros((width, height))
-    gx = src.copy()
-    gy = src.copy()
+    gx = createImg(height, width)
+    gy = createImg(height, width)
     #gy = np.zeros((width, height))
 
     for x in range(1, width-2):
         for y in range(1, height-2):
             gx[x, y] = (float(src[x+1, y]) - float(src[x-1, y]))/2.0
             gy[x, y] = (float(src[x, y+1]) - float(src[x, y-1]))/2.0
+            #if abs(gx[x,y]) <= 0.5 or abs(gy[x,y]) <= 0.5:
+                #print(str(src[x+1, y]) + "-" + str(src[x-1, y]))
+                #print(str(src[x, y+1]) + "-" + str(src[x, y-1]))
 
     return gx, gy
 