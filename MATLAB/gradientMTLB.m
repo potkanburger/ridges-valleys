9dbf1e4ee3cdf7f3ef08d0bd4029eaf750cb53c6
Sébastien Hu <potkanburger@users.noreply.github.com>
2019-09-20 09:40:28 +0200

    Gradient comparisons between Matlab and Python scripts

--- MATLAB/gradientMTLB.m
+++ MATLAB/gradientMTLB.m
@@ -5,9 +5,12 @@ I = rgb2gray(ITEMP);
 figure(1)
 imshowpair(Gx,Gy,'montage')
 title('Directional Gradients Gx and Gy, Using Central Difference')
+saveas(gcf,'lena_gradMATLAB.png')
 
 pyXgrad = imread('x_gradient_smooth_lena.jpg');
 pyYgrad = imread('y_gradient_smooth_lena.jpg');
 figure(2)
+
 imshowpair(pyXgrad,pyYgrad,'montage')
 title('Directional Gradients Gx and Gy, Using Central Difference, From Python')
+saveas(gcf,'lena_gradPYT.png')
